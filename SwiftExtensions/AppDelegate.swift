d9ae128e3af6560af531d1092d456da25d4e6cbe
Divya <divyam01986@gmail.com>
2020-11-11 16:57:11 -0500

    Setup Swift Extensions Project

---
+++ SwiftExtensions/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  SwiftExtensions
+//
+//  Created by Divya Mandyam on 11/11/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
