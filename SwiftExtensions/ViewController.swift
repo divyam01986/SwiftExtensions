759e85e6ea02571e528e12991192a226f74a6960
Divya <divyam01986@gmail.com>
2020-11-12 00:39:51 -0500

    Created Superscript/Subscript formatter function

--- SwiftExtensions/ViewController.swift
+++ SwiftExtensions/ViewController.swift
@@ -8,6 +8,20 @@ class ViewController: UIViewController {
         return vertStackView
     }()
     
+    private lazy var superscriptLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var subscriptLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
     private let scrollView = UIScrollView()
     
     override func viewDidLoad() {
@@ -18,9 +32,25 @@ class ViewController: UIViewController {
     
     private func layoutViews() {
         view.addSubview(scrollView)
-        scrollView.easy.layout(Edges(), Width(view.bounds.width))
+        scrollView.easy.layout(Top().to(view.safeAreaLayoutGuide, .top),
+                               Left(),
+                               Right(),
+                               Bottom(),
+                               Width(view.bounds.width))
         scrollView.addSubview(stackView)
         stackView.easy.layout(Edges())
+        
+        let text = "HelloYou"
+        superscriptLabel.attributedText = TextFormatter.superscriptText(text: text,
+                                                                        font: UIFont(name: "Helvetica Neue", size: 16.0)!,
+                                                                        color: UIColor.black, superscriptRange: NSRange(location: text.count - 3, length: 3))
+        stackView.addArrangedSubview(superscriptLabel)
+        
+        subscriptLabel.attributedText = TextFormatter.subscriptText(text: text,
+                                                                    font: UIFont(name: "Helvetica Neue", size: 16.0)!,
+                                                                    color: UIColor.black,
+                                                                    subscriptRange: NSRange(location: text.count - 3, length: 3))
+        stackView.addArrangedSubview(subscriptLabel)
     }
 }
 
